21ff834108d59752c160954a482b7867c5b72fe7
Keng Suppaseth <kengio@gmail.com>
2021-01-07 11:06:45 +0700

    Add query conditions and specs

--- app/api/v2/market/trades.rb
+++ app/api/v2/market/trades.rb
@@ -7,9 +7,15 @@ module API
       class Trades < Grape::API
         helpers API::V2::Market::NamedParams
 
+        helpers do
+          def opposite_type_param
+            params[:type] == 'buy' ? 'sell' : 'buy'
+          end
+        end
+
         desc 'Get your executed trades. Trades are sorted in reverse creation order.',
-          is_array: true,
-          success: API::V2::Entities::Trade
+             is_array: true,
+             success: API::V2::Entities::Trade
         params do
           optional :market,
                    values: { value: ->(v) { (Array.wrap(v) - ::Market.active.ids).blank? }, message: 'market.market.doesnt_exist' },
@@ -18,25 +24,11 @@ module API
         end
         get '/trades' do
           user_authorize! :read, ::Trade
-          # for sell orders query should be (when params[:type] == sell)
-          # where("taker_id = #{current_user.id} AND taker_type = 'sell' OR maker_id = #{current_user.id} AND taker_type = 'buy'")
-
-          # for buy orders query should be (when params[:type] == buy)
-          # where("taker_id = #{current_user.id} AND taker_type = 'buy' OR maker_id = #{current_user.id} AND taker_type = 'sell')
-
-          # If you want you can rewrite those filters with ransack (you can see a lot of examples on admin API endpoints)
-
-          # write specs on spec/market/trades_spec.rb
-
-          # it should be at least 4 tests
-          # 1) taker_id = current_user.id AND taker_type = 'sell' (should give you as trades with sell side)
-          # 2) maker_id = current_user.id AND taker_type = 'buy' (should give you as trades with sell side)
-          # 3) taker id = current_user.id AND taker_type = 'buy' (should give you as trades with buy side)
-          # 4) maker_id = current_user.id AND taker_type = 'sell' (should give you as trades with buy side)
 
           current_user
             .trades
             .order(order_param)
+            .tap { |q| q.where!('(taker_id = ? AND taker_type = ?) OR (maker_id = ? AND taker_type = ?)', current_user.id, params[:type], current_user.id, opposite_type_param) if params[:type] }
             .tap { |q| q.where!(market: params[:market]) if params[:market] }
             .tap { |q| q.where!('created_at >= ?', Time.at(params[:time_from])) if params[:time_from] }
             .tap { |q| q.where!('created_at < ?', Time.at(params[:time_to])) if params[:time_to] }
